0039e3f241484d0c9e5fed2b7467586a95c82348
German Alonso <german.aguiniga11@outlook.es>
2025-10-13 23:22:04 -0700

    50, Registration of new users

--- server.js
+++ server.js
@@ -1,47 +1,113 @@
-'use strict';
-require('dotenv').config();
-console.log('Session secret is:', process.env.SESSION_SECRET); // Add this line to debug
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const cors = require('cors');
-const session = require('express-session')
-const passport = require('passport')
-const mongo = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectId;
+"use strict";
+require("dotenv").config();
+console.log("Session secret is:", process.env.SESSION_SECRET); // Add this line to debug
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const mongo = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+const LocalStrategy = require("passport-local");
 
 const app = express();
 
 app.use(cors());
 
-app.set('view engine', 'pug');
-app.set('views', './views/pug');
+app.set("view engine", "pug");
+app.set("views", "./views/pug");
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: {secure: false}
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 fccTesting(app); // For fCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-myDB(async client => {
-  const myDataBase = await client.db('database').collection('users');
+myDB(async (client) => {
+  const myDataBase = await client.db("database").collection("users");
 
   // Be sure to change the title
-  app.route('/').get((req, res) => {
+  app.route("/").get((req, res) => {
     // Change the response to render the Pug template
-    res.render('index', {
-      title: 'Connected to Database',
-      message: 'Please login'
+    res.render("index", {
+      title: "Connected to Database",
+      message: "Please login",
+      showLogin: true,
+      showRegistration: true
     });
+  });
+
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect("/");
+  }
+
+  app
+    .route("/login")
+    .post(
+      passport.authenticate("local", { failureRedirect: "/" }),
+      (req, res) => {
+        res.redirect("/profile");
+      }
+    );
+
+  app.route("/logout").get((req, res) => {
+    req.logOut();
+    res.redirect("/");
+  });
+
+  app.route("/register").post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect("/");
+        } else {
+          myDataBase.insertOne(
+            {
+              username: req.body.username,
+              password: req.body.password,
+            },
+            (err, doc) => {
+              if (err) {
+                res.redirect("/");
+              } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+              }
+            }
+          );
+        }
+      });
+    },
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res, next) => {
+      res.redirect("/profile");
+    }
+  );
+
+  app.use((req, res, next) => {
+    res.status(404).type("text").send("Not Found");
+  });
+
+  app.route("/profile").get(ensureAuthenticated, (req, res) => {
+    res.render("/profile", { username: req.user.username });
   });
 
   // Serialization and deserialization here...
@@ -52,22 +118,40 @@ myDB(async client => {
   passport.deserializeUser(async (id, done) => {
     console.log(new ObjectId(id));
     try {
-      const doc = await myDataBase.findOne({_id: new ObjectId(id)});
+      const doc = await myDataBase.findOne({ _id: new ObjectId(id) });
       done(null, doc);
     } catch (err) {
       done(err, null);
     }
   });
 
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        console.log(`User ${username} attempted to log in.`);
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
+        if (password !== user.password) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    })
+  );
+
   // Be sure to add this...
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   });
-6
-}).catch(e => {
-  app.route('/').get((req, res) => {
-    res.render('index', { title: e, message: 'Unable to connect to database' });
+  6;
+}).catch((e) => {
+  app.route("/").get((req, res) => {
+    res.render("index", { title: e, message: "Unable to connect to database" });
   });
 });
 // app.listen out here...
